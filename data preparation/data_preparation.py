448b4837f249d8f3e028371c81733db63c518f4a
Josua Ehret <jehret6@stud.hs-offenburg.de>
2024-12-11 00:26:12 +0100

    cleaned

--- data preparation/data_preparation.py
+++ data preparation/data_preparation.py
@@ -3,22 +3,20 @@ import os
 from sklearn.model_selection import train_test_split
 import matplotlib.pyplot as plt
 from sklearn.model_selection import StratifiedKFold
-
-
+import numpy as np
 
 main_dir = r"C:\Brain Cancer Prediction\data\Images"
 out_dir = r"C:\Brain Cancer Prediction\data\CSV/data.csv"
-
-# wohin die 5fold dateien
 output_dir = r"C:\Brain Cancer Prediction\data\CSV"
+
 def create_csv_and_folds():
     meta_data = pd.DataFrame(columns=[
         "Name", "Path", "Cancer"
     ])
 
-
     for target in range(3):
         folder_path = os.path.join(main_dir, f"{target}")
+        print(f"Files in {folder_path}: {len(os.listdir(folder_path))}")
 
         for image_name in os.listdir(folder_path):
             image_path = os.path.join(folder_path,image_name)
@@ -35,38 +33,25 @@ def create_csv_and_folds():
     #-------------- Train Test split ---------------#
     X = meta_data.drop(columns=["Cancer"], inplace=False)
     y = meta_data["Cancer"]
+    y = pd.Series(y, dtype=np.int64)
+
+    assert len(X) == len(y), "X and y lengths do not match!"
+
     X_train, X_test, y_train, y_test = train_test_split(X,y, test_size=0.2)
-
-
-    #-------------- Train Val split ---------------#
-    X_train, X_valdiation, y_train, y_validation = train_test_split(X_train, y_train, test_size=0.2) 
-
-    # plots of the distribution
-    y_train.value_counts().sort_index().plot(kind="bar")
-    # plt.savefig("train_distribution")
-    y_test.value_counts().sort_index().plot(kind="bar")
-    # plt.savefig("test_distribution")
-    y_validation.value_counts().sort_index().plot(kind="bar")
-    # plt.savefig("validation_distribution")
-
-    df = pd.read_csv(out_dir)
-    train = df.drop(columns=["Cancer"])
-    test = df["Cancer"]
-
-    # split data in train and test
-    X_train, X_test, y_train, y_test = train_test_split(train, test, test_size=0.2)
 
     # save test set to dir
     test_data = pd.concat([X_test, y_test], axis=1)
     path_train = os.path.join(output_dir, "test_data.csv")
     test_data.to_csv(path_train ,index=False)
 
+    # save train set
     train_data = pd.concat([X_train, y_train], axis=1)
     path_train = os.path.join(output_dir, "train_data.csv")
     train_data.to_csv(path_train ,index=False)
 
-    X = X_train
-    y = y_train
+    # reset index
+    X = X_train.reset_index(drop=True)
+    y = y_train.reset_index(drop=True)
     skf = StratifiedKFold(n_splits=5, shuffle=True, random_state=42)
 
     # iterate over the stratified splits
@@ -75,18 +60,14 @@ def create_csv_and_folds():
         X_train, X_test = X.iloc[train_index], X.iloc[test_index]
         y_train, y_test = y.iloc[train_index], y.iloc[test_index]
 
-    
+
+        # create train data fold
         train_data = pd.concat([X_train, y_train], axis=1)
         path_train = os.path.join(output_dir, f"fold_{fold}_train.csv")
         train_data.to_csv(path_train ,index=False)
 
-        train_data["Cancer"].value_counts().plot(kind="bar")
-        # plt.savefig(os.path.join(plot_path, f"fold_{fold}_train_distribution"))
-
+        # create val data fold
         val_data = pd.concat([X_test, y_test], axis=1)
-        val_data["Cancer"].value_counts().plot(kind="bar")
-        # plt.savefig(os.path.join(plot_path, f"fold_{fold}_val_distribution"))
-
         path_val = os.path.join(output_dir,f"fold_{fold}_val.csv")
         val_data.to_csv(path_val, index=False)
 
